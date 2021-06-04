cbe243506bf3972852455f2221439b8c0c2f22bf
Carolaynebarret <carolaynebarreto@alu.uern.br>
2021-06-04 20:14:10 -0300

    ...

--- frontend/src/componets/NavBar/index.tsx
+++ frontend/src/componets/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import Img from 'assets/img/Empregos.png'
+import Img from 'assets/img/EMPREGOS (2).png'
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
