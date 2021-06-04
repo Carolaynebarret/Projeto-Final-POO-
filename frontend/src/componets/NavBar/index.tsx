fe7d201f1e46b01e764c0221862d1e82c4179435
Carolaynebarret <carolaynebarreto@alu.uern.br>
2021-06-04 19:32:51 -0300

    static

--- frontend/src/componets/NavBar/index.tsx
+++ frontend/src/componets/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import Img from 'assets/img/cv_102350 (1).png'
+import Img from 'assets/img/Empregos.png'
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
