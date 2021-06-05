be255d56f9a028cf27f98c2a28d8d5ec4ab362af
danielicapui <37754709+danielicapui@users.noreply.github.com>
2021-06-04 21:45:10 -0300

    Update Curriculo.java
    correção

--- backend/src/main/java/com/projetofinal/projetofinal/Curriculo.java
+++ backend/src/main/java/com/projetofinal/projetofinal/Curriculo.java
@@ -1,46 +1,19 @@
 #package com.uern.poo;
 
-public class Contato {
-    private String email;
-    private String telefone;
-    private String endereco;
-    private String link;
-    private String redesocial;
-    Contato(String email, String telefone, String endereco,String link,String redesocial){
-      this.email=email;
-      this.telefone=telefone;
-      this.endereco=endereco;
-      this.link=link;
-      this.redesocial=redesocial;
-    }
-    public String getEmail(){
-      return email;
-    }
-    public void setEmail(String email){
-      this.email=email;
-    }
-    public String getTelefone(){
-      return telefone;
-    }
-    public void setTelefone(String telefone){
-      this.telefone=telefone;
-    }
-    public String getEndereco(){
-      return endereco;
-    }
-    public void setEndereco(String endereco){
-      this.endereco=endereco;
-    }
-    public String getLink(){
-      return link;
-    }
-    public void setLink(String link){
-      this.link=link;
-    }
-    public String getRedesocial(){
-      return redesocial;
-    }
-    public void setRedesocial(String redesocial){
-      this.redesocial=redesocial;
-    }
+public class Curriculo {
+    private String experiencia;
+    private String formacao;
+    private String cargo_desejado;
+    private String aptidoes[];
+    private String objetivo;
+    private String especial[];
+    Curriculo(String experiencia,String formacao,String cargo_desejado, String aptidoes[],String objetivo, String especial[]){
+    this.experiencia=experiencia;
+    this.formacao=formacao;
+    this.cargo_desejado=cargo_desejado;
+    this.aptidoes=aptidoes;
+    this.objetivo=objetivo;
+    this.especial=especial;
+}
+  //metodos gets e sets
 }
