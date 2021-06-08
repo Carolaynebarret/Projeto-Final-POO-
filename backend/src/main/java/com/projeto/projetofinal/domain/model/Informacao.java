37e3e3346684053b3f400018fa2e6975e136a852
danielicapui <37754709+danielicapui@users.noreply.github.com>
2021-06-08 11:34:00 -0300

    Formatação
    pequena correção

--- backend/src/main/java/com/projeto/projetofinal/domain/model/Informacao.java
+++ backend/src/main/java/com/projeto/projetofinal/domain/model/Informacao.java
@@ -3,13 +3,11 @@ package com.projeto.projetofinal.domain.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class Informacao {
-	   private String nome;
+	    private String nome;
 	    private String sobrenome;
 	    private int data[];
 	    private String sexo;
-
 }
